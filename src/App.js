59edf37958bfb7e0d9f61cc54392c5c70ad0812a
Svalyan <you@example.com>
2023-01-19 14:23:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import head from './asset/head.png'; //
+import leftArm from './asset/left_arm.png';
+import leftHand from './asset/left_hand.png';
+import body from './asset/body.png';
+import rightArm from './asset/right_arm.png';
+import rightHand from './asset/right_hand.png';
+import leftLeg from './asset/left_leg.png';
+import leftFoot from './asset/left_foot.png';
+import leftShoes from './asset/left_shoes.png';
+import rightLeg from './asset/right_leg.png';
+import rightFoot from './asset/right_foot.png';
+import rightShoes from './asset/right_shoes.png';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={'wrap'}>
+        <div className="wrapHead">
+          <img src={head} alt={'head not find'} className={'headImg'}/>
+        </div>
+        <div className="wrapBody">
+          <div className={"wrapBodyLeftArm"}>
+            <img src={leftArm} alt={'leftArm not find'} className={'leftArm'}/>
+          </div>
+
+          <img src={body} alt={'body not find'} className={'bodyImg'}/>
+
+          <div className={"wrapBodyRightArm"}>
+            <img src={rightArm} alt={'rightArm not find'} className={'rightArm'}/>
+          </div>
+        <div className={"wrapLegs"}>
+            <img src={rightArm} alt={'rightArm not find'} className={'rightArm'}/>
+        </div>
+        </div>
+      </div>
   );
 }
 
